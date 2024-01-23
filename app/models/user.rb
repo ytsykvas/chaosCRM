e94106b318403b20c91a0a405a0c8d4d1f348e16
Yurii Tsykvas <tsykvasyurii@gmail.com>
2024-01-23 22:27:23 +0200

    Model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
 	validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
-	validates :phone, presence: true
-	validates :first_name, presence: true
-	validates :last_name, presence: true
+	validates :phone, presence: true, format: { with: /\A\+?\d{10,15}\z/ }
+	validates :first_name, presence: true, format: { with: /\A[\p{L}'][\p{L}'\-]{0,14}\z/u }
+	validates :last_name, presence: true, format: { with: /\A[\p{L}'][\p{L}'\-]{0,14}\z/u }
+	validates :account_type, inclusion: { in: %w(visitor admin employee) }
 end
