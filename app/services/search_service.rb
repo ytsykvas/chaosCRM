fb25384659a99987731baf24215d5814d478845d
Yurii Tsykvas <tsykvasyurii@gmail.com>
2024-02-09 19:36:53 +0200

    Refactored SearchService

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -38,8 +38,8 @@ class SearchService
                         entry.last_name.to_s.downcase.include?(term) ||
                         entry.phone.to_s.include?(term)
       relevance += 2 if entry.email.to_s.downcase.include?(term)
-      relevance += 1 if @visit == 'NoLastVisit' && entry.last_visit.nil?
-      relevance += 1 if @visit == 'LastVisitOverMonth' && entry.last_visit.present? && entry.last_visit < 1.month.ago
+      relevance += 1 if @visit == 'NoLastVisit' && entry.last_visit.nil? ||
+                        @visit == 'LastVisitOverMonth' && entry.last_visit.present? && entry.last_visit < 1.month.ago
 
       break if relevance.zero?
     end
